bea0bd10a7019944b3f7e36bd9ccf6488e1461ad
Milla Kelhu <milla.kelhu@gmail.com>
2022-05-19 12:59:58 +0300

    Exercise 2.11

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,17 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Persons from './components/Persons'
 import PersonQuery from './components/PersonQuery'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ newSearch, setNewSearch ] = useState('')
+
+  const hook = () => {
+    console.log('effect is being used')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('persons fetched')
+        setPersons(response.data)
+      })
+  }
+  useEffect(hook, [])
+  console.log('render', persons.length, 'persons')
 
   const addPerson = (event) => {
     event.preventDefault()
